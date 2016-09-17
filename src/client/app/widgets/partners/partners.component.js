98fc449accc8a7ae5229a5a5d47cbcfa390e8c2f
gion <bogdan.gradinariu@thinslices.com>
2016-09-17 04:11:07 +0300

    Implement footer functinality

--- src/client/app/widgets/partners/partners.component.js
+++ src/client/app/widgets/partners/partners.component.js
@@ -10,11 +10,46 @@
         function PartnersController() {
             var vm = this;
 
+            vm.sponsors = [
+                {
+                    name: 'Thinslices',
+                    url: 'http://www.thinslices.com/',
+                    // image: 'http://www.thinslices.com/wp-content/themes/thinslicescom_new/images/TS_logo_blue_website.svg'
+                    image: 'http://profs.info.uaic.ro/~itc2fii/Build-IT/images/colaboratori/thinslices.png'
+                },
+                {
+                    name: 'Antibiotice',
+                    url: 'http://www.antibiotice.ro/',
+                    image: 'http://www.antibiotice.ro/wp-content/themes/antibiotice/images/antibiotice-logo.png'
+                },
+                {
+                    name: 'Xivic',
+                    url: 'http://www.xivic.com/',
+                    image: 'https://pbs.twimg.com/profile_images/1125643544/xivicinc_400x400.jpg'
+                },
+                {
+                    name: 'Wedalert',
+                    url: 'https://www.facebook.com/wedalert.ro',
+                    image: 'https://scontent-fra3-1.xx.fbcdn.net/v/t1.0-1/10945720_1549941321950356_6088432585448441425_n.png?oh=32727bd63991be781958311f59ccec9e&oe=587A7846'
+                },
+                {
+                    name: 'Conest',
+                    url: 'http://www.conestiasi.ro',
+                    image: 'http://www.conestiasi.ro/app/img/logo_top.png'
+                }
+            ];
+
             vm.partners = [
-                {name: 'Thinslices'},
-                {name: 'Antibiotice'},
-                {name: 'Xivic'},
-                {name: 'Wedalert'}
+                {
+                    name: 'TVR Iasi',
+                    url: 'http://iasi.tvr.ro/',
+                    image: 'http://gardenerjazz.ro/2015/images/logos/Logo_TVRIasi.png'
+                },
+                {
+                    name: 'Studioul de Baza',
+                    url: 'https://www.facebook.com/StudioulDeBaza',
+                    image: 'http://gardenerjazz.ro/2015/images/logos/Logo_StudiouldeBaza.png'
+                }
             ];
         }
 
